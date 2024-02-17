3f8d413ad14eeb21e49fa0c0a45d7eb425a807b6
dexzea22 <77290837+dexzea22@users.noreply.github.com>
2024-02-17 19:39:10 +0800

    req host

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m91)q77#s!spk_&2w#^2dpkf!t6rsah0-9zzn+gr*xxl0!^@qs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['portfolio-2zpl.onrender.com']
 
