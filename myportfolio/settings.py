2d183e7289e838259de3308dec5375c8a02c2001
dexzea22 <77290837+dexzea22@users.noreply.github.com>
2024-02-17 19:11:06 +0800

    allow host

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m91)q77#s!spk_&2w#^2dpkf!t6rsah0-9zzn+gr*xxl0!^@qs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# Add your domain(s) to the ALLOWED_HOSTS list
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', 'dexportfolio.onrender.com']
 
 
 # Application definition
